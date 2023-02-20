62279985e48612eec77a4de7b7bf90c71a812590
Build Er <builder@macmini6.local>
2023-02-20 15:43:54 -0300

    Package version: 1.0.0-beta+20230220183523

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXUCAnimationViewWrapper",
 				dependencies: [
 					"GXUCAnimationView",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230217195542.xcframework.zip",
-			checksum: "3dbfd5cf3ed26850a9649a76ff2fc30aa25a7c0eb9f43c7ad4ab1211ed59dd43"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230220183523.xcframework.zip",
+			checksum: "b8cb9509193b260bb8bab162adf26bef1919cc93a83aa2e2cc5654e296bd0ef5"
 		)
 	]
 )
