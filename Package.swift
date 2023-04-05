cde078aa4649256a7311e30d0c91597ac9a8a218
Build Er <builder@macmini6.local>
2023-04-05 10:43:11 -0300

    Package version: 1.0.0-beta+20230405133406

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230330152753.xcframework.zip",
-			checksum: "114ccd439c22bd902e64934ff854192c79fdfac5c3810cb7b502805bb5c79f39"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230405133406.xcframework.zip",
+			checksum: "aacd93134f5368289655460e0c2cf6f2e9a7cb4af6dc84c6926ea9ffa4009c70"
 		)
 	]
 )
