d50b240558b14f9e4fb5486618568b15b94e9826
genexusbot <builder@genexus.com>
2023-05-15 14:55:08 -0300

    Package version: 1.0.0-beta+20230515174624

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230515153434.xcframework.zip",
-			checksum: "0c8ce0d5490c435a86f0ab70756dbc0a45f2ea7ab5a1f5c01866886819fbf820"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta+20230515174624.xcframework.zip",
+			checksum: "ae1256109518ab3f1d6157a8e20e93f8e977e8a3195c60b4d8cb6458ea58d3fb"
 		)
 	]
 )
