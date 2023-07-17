593a0989562b91ad98c54e271c123da01dc54ae7
genexusbot <builder@genexus.com>
2023-07-17 13:06:07 -0300

    Package version: 1.0.0-beta.20230717155910

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCAnimationViewWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-beta.20230714111921")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-beta.20230717155910")
 	],
 	targets: [
 		.target(name: "GXUCAnimationViewWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta.20230714111921.xcframework.zip",
-			checksum: "0ff46c8b16a9064a8e76c0f15360673673f91313cebfe158575508faa85701e2"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-1.0.0-beta.20230717155910.xcframework.zip",
+			checksum: "d393748c466948d2d087f6255e8701e2a849da6dab735d0208900a71f90e27a8"
 		)
 	]
 )
