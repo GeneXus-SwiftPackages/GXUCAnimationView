4082a3d4c805c7b7387a097e267dae3e80666dbf
Build Er <builder@macmini6.local>
2023-12-07 15:21:33 -0300

    Package version: 1.3.0

--- Package.swift
+++ Package.swift
@@ -10,19 +10,21 @@ let package = Package(
 			targets: ["GXUCAnimationViewWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0"),
+		.package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMinor(from: "4.3.0"))
 	],
 	targets: [
 		.target(name: "GXUCAnimationViewWrapper",
 				dependencies: [
 					"GXUCAnimationView",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS])),
+					.product(name: "Lottie", package: "lottie-spm", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXUCAnimationView-1.1.0.xcframework.zip",
-			checksum: "fdf8c7e754eaa049a91d76cffa2db1052c3efa750dfaa43dfa24edec546b82dc"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXUCAnimationView-1.3.0.xcframework.zip",
+			checksum: "3a2ffd59336798225697ff9df1c25d681bce9f4cfa6ae140de1fb96ab4ac3b44"
 		)
 	]
 )
