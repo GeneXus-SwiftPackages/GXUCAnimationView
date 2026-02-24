973841c847819c02a9fb08a0c1385a5c82cc423a
The iOS Team @ GeneXus <builder@genexus.com>
2026-02-24 09:01:51 -0500

    Package version: 4.1.0-beta.24

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCAnimationViewWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.23"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.24"),
 		.package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMinor(from: "4.4.3"))
 	],
 	targets: [
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCAnimationView",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-4.1.0-beta.23.xcframework.zip",
-			checksum: "235b2ce344d7793b72d963fa71d0ed031d95fd61319c0fdd68d576fa198057d5"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCAnimationView-4.1.0-beta.24.xcframework.zip",
+			checksum: "3994b8c1673d8fbc45302af4d6ed5ffb7adc6d8ed6740e22b6b0e93d9d9210a0"
 		)
 	]
 )
